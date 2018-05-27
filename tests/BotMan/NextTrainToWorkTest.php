cd0909d9c2c5f2e8066e0f9c9e22d93cc1e2a36b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-27 15:11:00 +0100

    Added additional test for the train to work

--- tests/BotMan/NextTrainToWorkTest.php
+++ tests/BotMan/NextTrainToWorkTest.php
@@ -25,4 +25,22 @@ class NextTrainToWorkTest extends TestCase
              ->receives('Get me the next train to work')
              ->assertReply('The next train is expected at 11:00');
     }
+
+    public function testGetTrainTimeAlexa()
+    {
+        $client = m::mock('Matthewbdaly\TransportApi\Contracts\Client');
+        $client->shouldReceive('getDepartures')
+               ->with('DIS', 'NRW')
+               ->once()
+               ->andReturn(new Response(200, [], json_encode([
+                   'departures' => [
+                       'all' => [
+                           ['expected_departure_time' => '11:00']
+                       ]]])));
+
+        $this->app->instance('Matthewbdaly\TransportApi\Contracts\Client', $client);
+        $this->bot
+             ->receives('nexttrain')
+             ->assertReply('The next train is expected at 11:00');
+    }
 }
