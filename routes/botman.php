fd30bed361e7ffec90584d292dce690731819cd6
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-27 11:54:37 +0100

    Removed unwanted routes

--- routes/botman.php
+++ routes/botman.php
@@ -3,10 +3,5 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
-$botman->hears('Get me a train time', BotManController::class.'@getTrainTime');
 $botman->hears('Get me the next train to work', BotManController::class.'@getNextTrainToWork');
 $botman->hears('nexttrain', BotManController::class.'@getNextTrainToWork');
